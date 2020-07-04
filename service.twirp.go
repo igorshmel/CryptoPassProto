5ab4b34508426400b5b14a692477fcd207e6feff
shmel <shmel@system32.ru>
2020-07-05 01:04:07 +0300

    fix imports

--- service.twirp.go
+++ service.twirp.go
@@ -8,7 +8,7 @@ This code was generated with github.com/twitchtv/twirp/protoc-gen-twirp v5.12.0.
 It is generated from these files:
 	service.proto
 */
-package __twproto
+package twproto
 
 import bytes "bytes"
 import strings "strings"
