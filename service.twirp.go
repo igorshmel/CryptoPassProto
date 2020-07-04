0f8a7bdf37b5777a466f9d7ac5d4266b0cd4f27c
shmel <shmel@system32.ru>
2020-07-05 00:20:31 +0300

    add Api Service

--- service.twirp.go
+++ service.twirp.go
@@ -8,7 +8,7 @@ This code was generated with github.com/twitchtv/twirp/protoc-gen-twirp v5.12.0.
 It is generated from these files:
 	service.proto
 */
-package twproto
+package __twproto
 
 import bytes "bytes"
 import strings "strings"
@@ -520,6 +520,322 @@ func (s *haberdasherServer) ProtocGenTwirpVersion() string {
 
 func (s *haberdasherServer) PathPrefix() string {
 	return HaberdasherPathPrefix
+}
+
+// =============
+// Api Interface
+// =============
+
+type Api interface {
+	Authorize(context.Context, *RequestAuthorization) (*ReplayAuthorization, error)
+}
+
+// ===================
+// Api Protobuf Client
+// ===================
+
+type apiProtobufClient struct {
+	client HTTPClient
+	urls   [1]string
+	opts   twirp.ClientOptions
+}
+
+// NewApiProtobufClient creates a Protobuf client that implements the Api interface.
+// It communicates using Protobuf and can be configured with a custom HTTPClient.
+func NewApiProtobufClient(addr string, client HTTPClient, opts ...twirp.ClientOption) Api {
+	if c, ok := client.(*http.Client); ok {
+		client = withoutRedirects(c)
+	}
+
+	clientOpts := twirp.ClientOptions{}
+	for _, o := range opts {
+		o(&clientOpts)
+	}
+
+	prefix := urlBase(addr) + ApiPathPrefix
+	urls := [1]string{
+		prefix + "Authorize",
+	}
+
+	return &apiProtobufClient{
+		client: client,
+		urls:   urls,
+		opts:   clientOpts,
+	}
+}
+
+func (c *apiProtobufClient) Authorize(ctx context.Context, in *RequestAuthorization) (*ReplayAuthorization, error) {
+	ctx = ctxsetters.WithPackageName(ctx, "twirp.example.haberdasher")
+	ctx = ctxsetters.WithServiceName(ctx, "Api")
+	ctx = ctxsetters.WithMethodName(ctx, "Authorize")
+	out := new(ReplayAuthorization)
+	err := doProtobufRequest(ctx, c.client, c.opts.Hooks, c.urls[0], in, out)
+	if err != nil {
+		twerr, ok := err.(twirp.Error)
+		if !ok {
+			twerr = twirp.InternalErrorWith(err)
+		}
+		callClientError(ctx, c.opts.Hooks, twerr)
+		return nil, err
+	}
+
+	callClientResponseReceived(ctx, c.opts.Hooks)
+
+	return out, nil
+}
+
+// ===============
+// Api JSON Client
+// ===============
+
+type apiJSONClient struct {
+	client HTTPClient
+	urls   [1]string
+	opts   twirp.ClientOptions
+}
+
+// NewApiJSONClient creates a JSON client that implements the Api interface.
+// It communicates using JSON and can be configured with a custom HTTPClient.
+func NewApiJSONClient(addr string, client HTTPClient, opts ...twirp.ClientOption) Api {
+	if c, ok := client.(*http.Client); ok {
+		client = withoutRedirects(c)
+	}
+
+	clientOpts := twirp.ClientOptions{}
+	for _, o := range opts {
+		o(&clientOpts)
+	}
+
+	prefix := urlBase(addr) + ApiPathPrefix
+	urls := [1]string{
+		prefix + "Authorize",
+	}
+
+	return &apiJSONClient{
+		client: client,
+		urls:   urls,
+		opts:   clientOpts,
+	}
+}
+
+func (c *apiJSONClient) Authorize(ctx context.Context, in *RequestAuthorization) (*ReplayAuthorization, error) {
+	ctx = ctxsetters.WithPackageName(ctx, "twirp.example.haberdasher")
+	ctx = ctxsetters.WithServiceName(ctx, "Api")
+	ctx = ctxsetters.WithMethodName(ctx, "Authorize")
+	out := new(ReplayAuthorization)
+	err := doJSONRequest(ctx, c.client, c.opts.Hooks, c.urls[0], in, out)
+	if err != nil {
+		twerr, ok := err.(twirp.Error)
+		if !ok {
+			twerr = twirp.InternalErrorWith(err)
+		}
+		callClientError(ctx, c.opts.Hooks, twerr)
+		return nil, err
+	}
+
+	callClientResponseReceived(ctx, c.opts.Hooks)
+
+	return out, nil
+}
+
+// ==================
+// Api Server Handler
+// ==================
+
+type apiServer struct {
+	Api
+	hooks *twirp.ServerHooks
+}
+
+func NewApiServer(svc Api, hooks *twirp.ServerHooks) TwirpServer {
+	return &apiServer{
+		Api:   svc,
+		hooks: hooks,
+	}
+}
+
+// writeError writes an HTTP response with a valid Twirp error format, and triggers hooks.
+// If err is not a twirp.Error, it will get wrapped with twirp.InternalErrorWith(err)
+func (s *apiServer) writeError(ctx context.Context, resp http.ResponseWriter, err error) {
+	writeError(ctx, resp, err, s.hooks)
+}
+
+// ApiPathPrefix is used for all URL paths on a twirp Api server.
+// Requests are always: POST ApiPathPrefix/method
+// It can be used in an HTTP mux to route twirp requests along with non-twirp requests on other routes.
+const ApiPathPrefix = "/twirp/twirp.example.haberdasher.Api/"
+
+func (s *apiServer) ServeHTTP(resp http.ResponseWriter, req *http.Request) {
+	ctx := req.Context()
+	ctx = ctxsetters.WithPackageName(ctx, "twirp.example.haberdasher")
+	ctx = ctxsetters.WithServiceName(ctx, "Api")
+	ctx = ctxsetters.WithResponseWriter(ctx, resp)
+
+	var err error
+	ctx, err = callRequestReceived(ctx, s.hooks)
+	if err != nil {
+		s.writeError(ctx, resp, err)
+		return
+	}
+
+	if req.Method != "POST" {
+		msg := fmt.Sprintf("unsupported method %q (only POST is allowed)", req.Method)
+		err = badRouteError(msg, req.Method, req.URL.Path)
+		s.writeError(ctx, resp, err)
+		return
+	}
+
+	switch req.URL.Path {
+	case "/twirp/twirp.example.haberdasher.Api/Authorize":
+		s.serveAuthorize(ctx, resp, req)
+		return
+	default:
+		msg := fmt.Sprintf("no handler for path %q", req.URL.Path)
+		err = badRouteError(msg, req.Method, req.URL.Path)
+		s.writeError(ctx, resp, err)
+		return
+	}
+}
+
+func (s *apiServer) serveAuthorize(ctx context.Context, resp http.ResponseWriter, req *http.Request) {
+	header := req.Header.Get("Content-Type")
+	i := strings.Index(header, ";")
+	if i == -1 {
+		i = len(header)
+	}
+	switch strings.TrimSpace(strings.ToLower(header[:i])) {
+	case "application/json":
+		s.serveAuthorizeJSON(ctx, resp, req)
+	case "application/protobuf":
+		s.serveAuthorizeProtobuf(ctx, resp, req)
+	default:
+		msg := fmt.Sprintf("unexpected Content-Type: %q", req.Header.Get("Content-Type"))
+		twerr := badRouteError(msg, req.Method, req.URL.Path)
+		s.writeError(ctx, resp, twerr)
+	}
+}
+
+func (s *apiServer) serveAuthorizeJSON(ctx context.Context, resp http.ResponseWriter, req *http.Request) {
+	var err error
+	ctx = ctxsetters.WithMethodName(ctx, "Authorize")
+	ctx, err = callRequestRouted(ctx, s.hooks)
+	if err != nil {
+		s.writeError(ctx, resp, err)
+		return
+	}
+
+	reqContent := new(RequestAuthorization)
+	unmarshaler := jsonpb.Unmarshaler{AllowUnknownFields: true}
+	if err = unmarshaler.Unmarshal(req.Body, reqContent); err != nil {
+		s.writeError(ctx, resp, malformedRequestError("the json request could not be decoded"))
+		return
+	}
+
+	// Call service method
+	var respContent *ReplayAuthorization
+	func() {
+		defer ensurePanicResponses(ctx, resp, s.hooks)
+		respContent, err = s.Api.Authorize(ctx, reqContent)
+	}()
+
+	if err != nil {
+		s.writeError(ctx, resp, err)
+		return
+	}
+	if respContent == nil {
+		s.writeError(ctx, resp, twirp.InternalError("received a nil *ReplayAuthorization and nil error while calling Authorize. nil responses are not supported"))
+		return
+	}
+
+	ctx = callResponsePrepared(ctx, s.hooks)
+
+	var buf bytes.Buffer
+	marshaler := &jsonpb.Marshaler{OrigName: true}
+	if err = marshaler.Marshal(&buf, respContent); err != nil {
+		s.writeError(ctx, resp, wrapInternal(err, "failed to marshal json response"))
+		return
+	}
+
+	ctx = ctxsetters.WithStatusCode(ctx, http.StatusOK)
+	respBytes := buf.Bytes()
+	resp.Header().Set("Content-Type", "application/json")
+	resp.Header().Set("Content-Length", strconv.Itoa(len(respBytes)))
+	resp.WriteHeader(http.StatusOK)
+
+	if n, err := resp.Write(respBytes); err != nil {
+		msg := fmt.Sprintf("failed to write response, %d of %d bytes written: %s", n, len(respBytes), err.Error())
+		twerr := twirp.NewError(twirp.Unknown, msg)
+		callError(ctx, s.hooks, twerr)
+	}
+	callResponseSent(ctx, s.hooks)
+}
+
+func (s *apiServer) serveAuthorizeProtobuf(ctx context.Context, resp http.ResponseWriter, req *http.Request) {
+	var err error
+	ctx = ctxsetters.WithMethodName(ctx, "Authorize")
+	ctx, err = callRequestRouted(ctx, s.hooks)
+	if err != nil {
+		s.writeError(ctx, resp, err)
+		return
+	}
+
+	buf, err := ioutil.ReadAll(req.Body)
+	if err != nil {
+		s.writeError(ctx, resp, wrapInternal(err, "failed to read request body"))
+		return
+	}
+	reqContent := new(RequestAuthorization)
+	if err = proto.Unmarshal(buf, reqContent); err != nil {
+		s.writeError(ctx, resp, malformedRequestError("the protobuf request could not be decoded"))
+		return
+	}
+
+	// Call service method
+	var respContent *ReplayAuthorization
+	func() {
+		defer ensurePanicResponses(ctx, resp, s.hooks)
+		respContent, err = s.Api.Authorize(ctx, reqContent)
+	}()
+
+	if err != nil {
+		s.writeError(ctx, resp, err)
+		return
+	}
+	if respContent == nil {
+		s.writeError(ctx, resp, twirp.InternalError("received a nil *ReplayAuthorization and nil error while calling Authorize. nil responses are not supported"))
+		return
+	}
+
+	ctx = callResponsePrepared(ctx, s.hooks)
+
+	respBytes, err := proto.Marshal(respContent)
+	if err != nil {
+		s.writeError(ctx, resp, wrapInternal(err, "failed to marshal proto response"))
+		return
+	}
+
+	ctx = ctxsetters.WithStatusCode(ctx, http.StatusOK)
+	resp.Header().Set("Content-Type", "application/protobuf")
+	resp.Header().Set("Content-Length", strconv.Itoa(len(respBytes)))
+	resp.WriteHeader(http.StatusOK)
+	if n, err := resp.Write(respBytes); err != nil {
+		msg := fmt.Sprintf("failed to write response, %d of %d bytes written: %s", n, len(respBytes), err.Error())
+		twerr := twirp.NewError(twirp.Unknown, msg)
+		callError(ctx, s.hooks, twerr)
+	}
+	callResponseSent(ctx, s.hooks)
+}
+
+func (s *apiServer) ServiceDescriptor() ([]byte, int) {
+	return twirpFileDescriptor0, 1
+}
+
+func (s *apiServer) ProtocGenTwirpVersion() string {
+	return "v5.12.0"
+}
+
+func (s *apiServer) PathPrefix() string {
+	return ApiPathPrefix
 }
 
 // =====
@@ -1038,20 +1354,27 @@ func callClientError(ctx context.Context, h *twirp.ClientHooks, err twirp.Error)
 }
 
 var twirpFileDescriptor0 = []byte{
-	// 240 bytes of a gzipped FileDescriptorProto
-	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xe2, 0xe2, 0x2d, 0x4e, 0x2d, 0x2a,
-	0xcb, 0x4c, 0x4e, 0xd5, 0x2b, 0x28, 0xca, 0x2f, 0xc9, 0x17, 0x92, 0x2c, 0x29, 0xcf, 0x2c, 0x2a,
-	0xd0, 0x4b, 0xad, 0x48, 0xcc, 0x2d, 0xc8, 0x49, 0xd5, 0xcb, 0x48, 0x4c, 0x4a, 0x2d, 0x4a, 0x49,
-	0x2c, 0xce, 0x48, 0x2d, 0x52, 0x52, 0xe5, 0x62, 0x0f, 0xc8, 0x29, 0x2d, 0x0e, 0x4a, 0x2d, 0x14,
-	0xe2, 0xe1, 0x62, 0x4c, 0x94, 0x60, 0x54, 0x60, 0xd4, 0x60, 0x0d, 0x62, 0x4c, 0x04, 0xf1, 0x92,
-	0x24, 0x98, 0x20, 0xbc, 0x24, 0x25, 0x45, 0xa8, 0xb2, 0x82, 0x1c, 0x21, 0x31, 0x2e, 0xb6, 0xc4,
-	0xbc, 0xe2, 0xf2, 0xd4, 0x22, 0xa8, 0x5a, 0x28, 0x4f, 0x49, 0x8e, 0x8b, 0x25, 0x38, 0xb3, 0x2a,
-	0x15, 0x24, 0x9f, 0x99, 0x97, 0x9c, 0x91, 0x5a, 0x0c, 0x93, 0x87, 0xf0, 0x94, 0xdc, 0xb9, 0x98,
-	0x3d, 0x12, 0x4b, 0x70, 0x49, 0x0b, 0x89, 0x70, 0xb1, 0x26, 0xe7, 0xe7, 0xe4, 0x17, 0x81, 0xed,
-	0xe4, 0x0c, 0x82, 0x70, 0x84, 0x84, 0xb8, 0x58, 0xf2, 0x12, 0x73, 0x53, 0x25, 0x98, 0xc1, 0x82,
-	0x60, 0xb6, 0xd1, 0x4a, 0x46, 0x2e, 0x6e, 0x0f, 0x84, 0x17, 0x84, 0xbc, 0xb8, 0xd8, 0x7d, 0x13,
-	0xb3, 0x53, 0x41, 0x86, 0xcb, 0xeb, 0xe1, 0xf4, 0xa9, 0x1e, 0xc8, 0x71, 0x52, 0x72, 0x78, 0x14,
-	0x80, 0x0c, 0xf0, 0xe3, 0x62, 0x01, 0xf9, 0x53, 0x48, 0x09, 0x8f, 0x3a, 0x68, 0x78, 0x49, 0x11,
-	0x54, 0x53, 0x90, 0xe3, 0xc4, 0x1d, 0xc5, 0xa9, 0x67, 0x5d, 0x52, 0x0e, 0x8e, 0x86, 0x24, 0x36,
-	0x30, 0x65, 0x0c, 0x08, 0x00, 0x00, 0xff, 0xff, 0x7c, 0x14, 0x9a, 0x60, 0x9e, 0x01, 0x00, 0x00,
+	// 339 bytes of a gzipped FileDescriptorProto
+	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x84, 0x92, 0xcd, 0x4a, 0xc3, 0x40,
+	0x14, 0x85, 0x89, 0xfd, 0xb3, 0xb7, 0xba, 0x19, 0x8b, 0xc4, 0x2c, 0xaa, 0x06, 0x04, 0x57, 0x11,
+	0xea, 0xd2, 0x55, 0xdd, 0x58, 0x04, 0x45, 0xe2, 0xce, 0x8d, 0xdc, 0xa4, 0x17, 0x33, 0x38, 0xcd,
+	0x4c, 0x67, 0x52, 0xa3, 0x7d, 0x23, 0xdf, 0x52, 0x26, 0x99, 0xd6, 0x45, 0x6d, 0x5c, 0x25, 0x1f,
+	0xe7, 0xcc, 0xe1, 0xfe, 0xc1, 0xa1, 0x21, 0xfd, 0xc1, 0x53, 0x8a, 0x94, 0x96, 0x85, 0x64, 0x27,
+	0x45, 0xc9, 0xb5, 0x8a, 0xe8, 0x13, 0xe7, 0x4a, 0x50, 0x94, 0x61, 0x42, 0x7a, 0x86, 0x26, 0x23,
+	0x1d, 0x4e, 0x61, 0x18, 0xd3, 0x62, 0x49, 0xa6, 0x98, 0x2c, 0x8b, 0x4c, 0x6a, 0xbe, 0xc2, 0x82,
+	0xcb, 0x9c, 0x0d, 0xa1, 0x43, 0x73, 0xe4, 0xc2, 0xf7, 0xce, 0xbc, 0xcb, 0x7e, 0x5c, 0x03, 0x0b,
+	0x60, 0x5f, 0xa1, 0x31, 0xa5, 0xd4, 0x33, 0x7f, 0xaf, 0x12, 0x36, 0x1c, 0xbe, 0xc2, 0x51, 0x4c,
+	0x4a, 0xe0, 0xd7, 0x76, 0x90, 0xd6, 0xb9, 0xac, 0x82, 0x3a, 0x71, 0x0d, 0xcc, 0x87, 0x1e, 0x69,
+	0x9d, 0xca, 0x19, 0xb9, 0x9c, 0x35, 0x3a, 0x45, 0x60, 0xfe, 0xe6, 0xb7, 0x36, 0x8a, 0xc5, 0xf0,
+	0x02, 0x7a, 0x4f, 0x62, 0x69, 0x62, 0x5a, 0xb0, 0x03, 0xf0, 0xd0, 0x05, 0x7a, 0x68, 0x29, 0xa9,
+	0x62, 0x3a, 0xb1, 0x97, 0x84, 0xe7, 0xce, 0xa6, 0x04, 0x3b, 0x86, 0x2e, 0xe6, 0xa6, 0x24, 0xed,
+	0xbc, 0x8e, 0xc2, 0x11, 0xb4, 0x9f, 0xf9, 0x8a, 0xac, 0xce, 0xf3, 0x34, 0x23, 0xb3, 0xd6, 0x6b,
+	0x0a, 0xef, 0xa0, 0x35, 0xc5, 0x62, 0x97, 0x6c, 0x5b, 0x4a, 0xa5, 0x90, 0xda, 0x95, 0x5e, 0x03,
+	0x63, 0xd0, 0xce, 0x71, 0x4e, 0xae, 0xea, 0xea, 0x7f, 0xfc, 0xed, 0xc1, 0x60, 0xfa, 0x3b, 0x6d,
+	0x76, 0x0f, 0xbd, 0x07, 0x7c, 0x27, 0x1b, 0x7e, 0x1a, 0xed, 0x5c, 0x4a, 0x64, 0x8b, 0x0b, 0x46,
+	0x0d, 0x06, 0x1b, 0xf0, 0x08, 0x6d, 0xdb, 0x27, 0x0b, 0x1b, 0x7c, 0x6e, 0x5e, 0xc1, 0xbf, 0x1e,
+	0x25, 0xc6, 0x06, 0x5a, 0x13, 0xc5, 0x99, 0x80, 0xfe, 0x7a, 0x81, 0xc4, 0xae, 0x1a, 0xde, 0xfd,
+	0x75, 0x36, 0x41, 0xd4, 0xf8, 0x60, 0xeb, 0x3a, 0x6e, 0x07, 0x2f, 0xfd, 0xe8, 0xa6, 0x28, 0xab,
+	0x33, 0x4d, 0xba, 0xd5, 0xe7, 0xfa, 0x27, 0x00, 0x00, 0xff, 0xff, 0x0f, 0x12, 0xb3, 0x12, 0xbe,
+	0x02, 0x00, 0x00,
 }
